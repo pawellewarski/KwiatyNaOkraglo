4b6db3e3678dde2ac5b17b1b78569b6a984f3654
pawellewarski <pawellewarski@gmail.com>
2018-10-22 19:43:02 +0200

    first commit

---
+++ src/main/java/pl/pawellewarski/Kwiaty/KwiatyApplication.java
@@ -0,0 +1,12 @@
+package pl.pawellewarski.Kwiaty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwiatyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwiatyApplication.class, args);
+	}
+}
