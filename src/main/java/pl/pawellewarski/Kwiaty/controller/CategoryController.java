032ed7de1f0c0d3e2f299548e2bd21163a6de612
pawellewarski <pawellewarski@gmail.com>
2018-10-27 15:43:40 +0200

    searching by categoty added

--- src/main/java/pl/pawellewarski/Kwiaty/controller/CategoryController.java
+++ src/main/java/pl/pawellewarski/Kwiaty/controller/CategoryController.java
@@ -3,14 +3,15 @@ package pl.pawellewarski.Kwiaty.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import pl.pawellewarski.Kwiaty.model.entities.Category;
+import pl.pawellewarski.Kwiaty.model.entities.Post;
 import pl.pawellewarski.Kwiaty.repository.CategoryRepository;
+import pl.pawellewarski.Kwiaty.repository.PostRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CategoryController {
@@ -18,8 +19,11 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/addCategory")
-    public String addPost(){
+    public String addPost() {
 
         return "addCategory";
     }
@@ -32,7 +36,8 @@ public class CategoryController {
         List<Category> categoryList = new ArrayList<>();
         Iterable<Category> categoryIterable = categoryRepository.findAll();
         for (Category category : categoryIterable) {
-            categoryList.add(category);}
+            categoryList.add(category);
+        }
         model.addAttribute("categories", categoryList);
 
         Category category = new Category(title, imgHtml, categoryDescription);
@@ -41,5 +46,27 @@ public class CategoryController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/category/{id}")
+    public String post(@PathVariable Long id,
+                       Model model) {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+
+        categoryOptional.ifPresent(category -> {
+            model.addAttribute("category", category);
+        });
+
+
+        List<Post> postList = new ArrayList<>();
+        Iterable<Post> postIterable = postRepository.findAll();
+        for (Post post : postIterable) {
+            if (post.getCategories().contains(categoryRepository.getOne(id)))
+                postList.add(post);
+        }
+        model.addAttribute("posts", postList);
+
+        return "categories";
+    }
+
 
 }
